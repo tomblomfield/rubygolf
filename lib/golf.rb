26e3bfc531eece5d7e532befa75f8215e212bc4a
Tom Blomfield <tomblomfield@gmail.com>
2011-05-09 19:46:33 +0100

    tom's push

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,2 +1,14 @@
 class Golf
+  
+  def self.hole1(a)
+    a.reduce(1, :*)
+  end
+  
+  def self.hole6(a)
+    (1..a).each.map{|e| e%15 == 0 ? "fizzbuzz" : (e%5 == 0 ? "buzz" : (e%3 == 0 ? "fizz" : e)) }
+  end
+  
+  def self.hole7(a)
+    a.reduce([[]]) {|m,e| m[-1][-1] == e-1 ? m[-1] << e : m << [e]; m}[1..-1].map{|a| a.length == 1 ? a[0].to_s : "#{a[0]}-#{a[-1]}"}    
+  end
 end
